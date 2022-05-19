5ad94a373f9af7621dbb8eea1e93766d204a3351
zsmya <877279245@qq.com>
2022-05-19 14:26:34 +0800

    update add package

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { createApp } from "vue"
 import App from "./App.vue"
 import { createPinia } from "pinia"
 import router from "./router"
+
+// 样式初始化
+import "normalize.css"
 const app = createApp(App)
 
 app.use(createPinia())
---
+++ src/utils/index.ts
@@ -0,0 +1,14 @@
+/*
+ * @Author: zsmya
+ * @Date: 2022-05-18 16:00:00
+ * @LastEditors: zsmya
+ * @LastEditTime: 2022-05-18 16:01:55
+ * @FilePath: /vite_ts_template/src/utils/index.ts
+ * @Description: 工具函数
+ * Copyright (c) 2022 by zsmya, All Rights Reserved.
+ */
+
+/**
+ * @description 深拷贝
+ */
+export const deepClone = (original: Array<any> | Object) => {}
